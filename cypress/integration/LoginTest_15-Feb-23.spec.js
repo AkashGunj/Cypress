81597f7ce966d0637b4510b69666623df0b8594d
akash <akashgunjal3644@gmail.com>
2023-02-15 13:38:26 +0530

    this is my 2nd commit

---
+++ cypress/integration/LoginTest_15-Feb-23.spec.js
@@ -0,0 +1,15 @@
+///<reference types = 'cypress'/>
+
+describe('validate the login functionality',function(){
+
+    it('Verify login functionality with valid cred',function(){
+        cy.visit('https://practicetestautomation.com/practice-test-login/')
+        cy.get('#username').type('student')
+        cy.get('#password').type('Password123')
+        cy.get('#submit').click()
+        cy.contains('Logged In Successfully').should('be.visible')
+        cy.contains('Congratulations student. You successfully logged in!').should('have.text','Congratulations student. You successfully logged in!')
+        cy.url().should('contain','https://practicetestautomation.com/logged-in-successfully/')
+        cy.contains('Log out').should('be.visible')
+    })
+})
